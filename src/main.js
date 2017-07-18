50c4e7effc9f8f710accf4ddc5caa4158e863e41
Bryan King <bpking15@gmail.com>
2017-07-17 22:15:52 -0400

    Wired in simplecast data + created shell of component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import moment from 'moment'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('MMMM DD, YYYY')
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
